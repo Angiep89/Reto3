4f6ae97bd3842cc53cc04ae7d55299c233cf6ff6
Angie <angie.perdomo.mt@correo.usa.edu.co>
2021-10-28 20:01:43 -0500

    primer commit

---
+++ src/main/java/AP_Reto3/Reto3/InterfaceBoat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package AP_Reto3.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Angiep
+ */
+public interface InterfaceBoat extends CrudRepository<Boat,Integer> {
+    
+}
